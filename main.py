5caf0cc7a93b45b4952c2c653a2ec3f2b660b1af
nienting <asdfg0926666533>
2022-05-06 17:27:40 +0800

    20220506

--- main.py
+++ main.py
@@ -1,91 +1,120 @@
+import flask
+import datetime
+
 from microblog import app, db
 from flask import request
 from microblog.models import User
-import datetime
+
+
+def message(item):
+    return flask.jsonify(
+        {
+            "id": item.id,
+            "title": item.title,
+            "should_alert": item.should_alert,
+            "scheduled_time": item.scheduled_time,
+            "done": item.done,
+            "created_at": item.create_at,
+            "updated_at": item.update_at
+            }
+    )
 
 
 @app.route('/')
 def homepage():
-    return 'Hello, World!'
+    return flask.jsonify('Hello, World!')
 
 
 @app.route('/todo_list/', methods=['POST'])
 def post_todo_list():
-    title = request.json['title']
-    scheduled_time = request.json['scheduled_time']
-    should_alert = request.json['should_alert']
-    todolist = User(title=title, scheduled_time=scheduled_time, should_alert=should_alert)
+    param = request.get_json()
+    if not param['title'] or not param['scheduled_time'] or not param['should_alert']:
+        return flask.jsonify(f"缺少..."), 401
+    todolist = User(title=param['title'], scheduled_time=param['scheduled_time'], should_alert=param['should_alert'])
     db.session.add(todolist)
     db.session.commit()
-    return 'Post OK'
+    return message(todolist)
 
 
-@app.route('/todo_list/<int:todo_list_id>', methods=['GET', 'PUT', 'DELETE'])
-def todo_list(todo_list_id):
-    if request.method == 'GET':
-        item = User.query.filter_by(id=todo_list_id).first_or_404('Item not found.')
-        print(item.id, item.title, item.scheduled_time, item.should_alert)
-        return 'Get OK'
-
-    elif request.method == 'PUT':
-        param = request.get_json()
-        item = User.query.filter_by(id=todo_list_id).first_or_404('Item not found.')
-        db.session.query(User).filter_by(id=todo_list_id).update(param)
-        db.session.add(item)
-        db.session.commit()
-        return 'Put OK'
-
-    else:
-        item = User.query.get(todo_list_id)
-        if item:
-            db.session.delete(item)
-            db.session.commit()
-            return 'Delete OK'
-        else:
-            return 'Item not found.'
+@app.route('/todo_list/<int:todo_list_id>', methods=['GET'])
+def todo_list_get(todo_list_id):
+    item = User.query.filter_by(id=todo_list_id).first()
+    if item is None:
+        return flask.jsonify('Item not found.'), 404
+    return message(item)
 
 
-@app.route('/todo_list/mark_done/<int:todo_list_id>', methods=['PUT'])
-def mark_done(todo_list_id):
-    item = User.query.filter_by(id=todo_list_id).first_or_404('Item not found.')
-    item.done = True
+@app.route('/todo_list/<int:todo_list_id>', methods=['PUT'])
+def todo_list_put(todo_list_id):
+    param = request.get_json()
+    if not param['title'] or not param['scheduled_time'] or not param['should_alert']:
+        return flask.jsonify(f"缺少..."), 401
+    item = User.query.filter_by(id=todo_list_id).first()
+    if item is None:
+        return flask.jsonify('Item not found.'), 404
+    db.session.query(User).filter_by(id=todo_list_id).update(param)
     db.session.add(item)
     db.session.commit()
-    return 'mark_done OK'
+    return message(item)
 
 
-@app.route('/todo_list/mark_undone/<int:todo_list_id>', methods=['PUT'])
-def mark_undone(todo_list_id):
-    item = User.query.filter_by(id=todo_list_id).first_or_404('Item not found.')
-    item.done = False
+@app.route('/todo_list/<int:todo_list_id>', methods=['DELETE'])
+def todo_list_delete(todo_list_id):
+    item = User.query.filter_by(id=todo_list_id).first()
+    if item is None:
+        return flask.jsonify('Item not found.'), 404
+    db.session.delete(item)
+    db.session.commit()
+    return flask.jsonify({"result": True})
+
+
+@app.route('/todo_list/<done_or_undone>/<int:todo_list_id>', methods=['PUT'])
+def mark_done_or_undone(done_or_undone, todo_list_id):
+    item = ""
+    if done_or_undone == "mark_done":
+        item = User.query.filter_by(id=todo_list_id).first()
+        if item is None:
+            return flask.jsonify('Item not found.'), 404
+        item.done = True
+    elif done_or_undone == "mark_undone":
+        item = User.query.filter_by(id=todo_list_id).first()
+        if item is None:
+            return flask.jsonify('Item not found.'), 404
+        item.done = False
     db.session.add(item)
     db.session.commit()
-    return 'mark_undone OK'
+    return message(item)
 
 
 @app.route('/todo_list/today', methods=['GET'])
 def todo_list_today():
     day = str(datetime.date.today())
-    item = User.query.filter(User.scheduled_time.startswith(day)).all()
-    if item:
-        for i in range(len(item)):
-            print(item[i].id, item[i].title, item[i].scheduled_time, item[i].should_alert, item[i].done,
-                  item[i].create_at, item[i].update_at)
-        return "today_list OK"
-    else:
-        return "No todo_list today"
+    return flask.jsonify([
+        {
+            "id": item.id,
+            "title": item.title,
+            "should_alert": item.should_alert,
+            "scheduled_time": item.scheduled_time,
+            "done": item.done,
+            "created_at": item.create_at,
+            "updated_at": item.update_at
+        } for item in User.query.filter(User.scheduled_time.startswith(day)).all()
+    ])
 
 
 @app.route('/todo_list/all', methods=['GET'])
 def todo_list_all():
-    all = User.query.all()
-    if all:
-        for i in range(len(all)):
-            print(all[i].id, all[i].title, all[i].scheduled_time, all[i].should_alert, all[i].done,
-                  all[i].create_at, all[i].update_at)
-        return "all_list OK"
-    else:
-        return "No todo_list"
+    return flask.jsonify([
+        {
+            "id": item.id,
+            "title": item.title,
+            "should_alert": item.should_alert,
+            "scheduled_time": item.scheduled_time,
+            "done": item.done,
+            "created_at": item.create_at,
+            "updated_at": item.update_at
+        } for item in User.query.all()
+    ])
 
 
 if __name__ == '__main__':
